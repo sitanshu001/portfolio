065155a50fc3f136ee030792a06bea59bff8e814
sitanshu001 <168004492+sitanshu001@users.noreply.github.com>
2025-02-27 23:40:22 +0530

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,144 @@ document.addEventListener("DOMContentLoaded", function () {
         span.style.animationDelay = `${j * 0.7}s`; // Apply delay for each span (e.g., 0s, 0.5s, 1s, ...)
     });
 });
+var e = document.getElementById("edu");
+var s = document.getElementById("spf");
+var p = document.getElementById("prj");
+var c = document.getElementById("crr");
+var bdy = document.getElementById("bdy");
+e.style.display = "none"
+s.style.display = "none"
+c.style.display = "none"
+p.style.display = "none"
+
+function edutgg() {
+    s.style.display = "none"
+    c.style.display = "none"
+    p.style.display = "none"
+    if (e.style.display === "none") {
+        // bdy.style.display = "block";s
+        e.style.display = "block";
+        
+    }
+    else if (e.style.display === "block"){
+        e.style.display = "none";
+
+    }
+    else{
+
+    }
+}
+
+function spftgg() {
+    e.style.display = "none"
+    c.style.display = "none"
+    p.style.display = "none"
+
+    if(s.style.display === "none"){
+        s.style.display = "block";
+        
+    }
+    else if (s.style.display === "block"){
+        s.style.display = "none";
+
+    }
+    else{
+        
+    }
+}
+function prjtgg() {
+    e.style.display = "none"
+    c.style.display = "none"
+    s.style.display = "none"
+
+    if(p.style.display === "none"){
+        p.style.display = "block";
+        
+    }
+    else if (p.style.display === "block"){
+        p.style.display = "none";
+
+    }
+    else{
+        
+    }
+}
+function crrtgg() {
+    e.style.display = "none"
+    s.style.display = "none"
+    p.style.display = "none"
+
+    if(c.style.display === "none"){
+        c.style.display = "block";
+        
+    }
+    else if (c.style.display === "block"){
+        c.style.display = "none";
+
+    }
+    else{
+        
+    }
+}
+    // } else {
+        // bdy.style.display = "block";
+        // e.style.display = "none";
+        // s.style.display = "none";
+        // p.style.display = "none";
+        // c.style.display = "none";
+    // }
+    // if (s.style.display === "none") {
+    //     // bdy.style.display = "block";s
+    //     s.style.display = "block";
+    //     e.style.display = "none"
+    //     c.style.display = "none"
+    //     p.style.display = "none"
+    // }
+  
+//   function myFunction() {
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
+
+// function prflfldToggle() {
+
+    // if (e.style.display === "none") {
+    //   e.style.display = "block";
+    // }
+    // if(e.style.display === "block"){
+    //     s.style.display = "none";
+    //     p.style.display = "none";
+    //     c.style.display = "none";
+    // }
+    // if (s.style.display === "none") {
+    //     s.style.display = "block";
+    // }
+    // if(s.style.display === "block"){
+    //     e.style.display = "none";
+    //     p.style.display = "none";
+    //     c.style.display = "none";
+    // }
+
+    // else if(s.style.display === "none"){
+    //   e.style.display = "none";
+    //   s.style.display = "block";
+    //   p.style.display = "none";
+    //   c.style.display = "none";
+
+    // }
+    // else if (c.style.display === "none") {
+    //     c.style.display = "block";
+    //     s.style.display = "none";
+    //     p.style.display = "none";
+    //     e.style.display = "none";
+    // }
+    // else if (p.style.display === "none") {
+    //     p.style.display = "block";
+    //     s.style.display = "none";
+    //     e.style.display = "none";
+    //     c.style.display = "none";
+    // }
+    
